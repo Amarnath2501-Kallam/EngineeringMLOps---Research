4e68a864c4795303a3fb03898b821849522a02e1
Srikanth Reddy <srikanthreddy@Srikanths-MacBook-Pro.local>
2021-09-13 11:21:33 +0100

    Add existing file

--- Model_Serving_Monitoring/inference.py
+++ Model_Serving_Monitoring/inference.py
@@ -1,19 +1,71 @@
-
-import json
 import requests
+import json
+import os
+import ssl
 import pandas as pd
 
+ 
 
-data = pd.read_csv('sample_inference_data.csv')
-data = data.drop(columns=['Timestamp', 'Location', 'Future_weather_condition'])
+def allowSelfSignedHttps(allowed):
 
-url = '----insert----your---endpoint---url-----'
-headers = {'Content-Type':'application/json'}
+# bypass the server certificate verification on client side
 
+    if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
+
+        ssl._create_default_https_context = ssl._create_unverified_context
+
+ 
+
+    allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+
+ 
+
+# Request data goes here
+
+ 
+
+data = pd.read_csv('validation_data.csv')
+
+# you need to drop 4 columns for this to work.
+
+data = data.drop(columns=['Timestamp', 'Location', 'Future_weather_condition', 'Pressure_millibars'])
+
+ 
+
+url = ''
+
+api_key = '' # Replace this with the API key for the web service
+
+headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+
+ 
 
 for i in range(len(data)):
-            inference_data = data.values[i].tolist()
-            inference_data = json.dumps({"data": [inference_data]})
-            r = requests.post(url, data=inference_data, headers=headers)
-            print(str(i)+str(r.content))
 
+    inference_data = data.values[i].tolist()
+
+    inference_data = json.dumps({"data": [inference_data]})
+
+    inference_data = str.encode(inference_data)
+
+ 
+
+    try:
+
+        response = requests.request("POST", url, headers=headers, data=inference_data)
+
+        result = response.text
+
+        print(result)
+
+    except requests.HTTPError as error:
+
+        print("The request failed with status code: " + str(error.code))
+
+ 
+
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+
+        print(error.info())
+
+        print(json.loads(error.read().decode("utf8", 'ignore')))
